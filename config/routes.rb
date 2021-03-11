e2eb157b072d630417a0b0ff6cedac4773791ef2
Phan Minh Huy <phanminhhuy1509@gmail.com>
2021-03-11 16:01:21 +0700

    Chapter_8: Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     root "static_pages#home"
     get "static_pages/home"
     get "static_pages/help"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
     resources :users, only: [:show, :new, :create]
   end
 end
