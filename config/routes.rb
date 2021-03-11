21b6f41fdb79c362881bee6f137c841318faeec9
Phan Minh Huy <phanminhhuy1509@gmail.com>
2021-03-11 16:01:21 +0700

    Chapter_7: Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     root "static_pages#home"
     get "static_pages/home"
     get "static_pages/help"
+    resources :users, only: [:show, :new, :create]
   end
 end
