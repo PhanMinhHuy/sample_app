7a17086f3ea6bdf2733af41d1a0ac9dac320ed5e
Phan Minh Huy <phanminhhuy1509@gmail.com>
2021-03-16 16:33:24 +0700

    Chapter_12: Password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,64 @@
+class PasswordResetsController < ApplicationController
+  before_action :load_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
+  def new; end
+
+  def create
+    @user = User.find_by email: params[:password_reset][:email].downcase
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = t "flash.password_reset_message"
+      redirect_to root_path
+    else
+      flash.now[:danger] = t "flash.not_found_email"
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if user_params[:password].blank?
+      @user.errors.add :password, t("password_blank")
+      render :edit
+    elsif @user.update user_params
+      log_in @user
+      @user.update_column :reset_digest, nil
+      flash[:success] = t "flash.password_reset_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :password, :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by email: params[:email]
+    return if @user
+
+    flash[:danger] = t "flash.not_found_user"
+    redirect_to new_user_path
+  end
+
+  def valid_user
+    return if @user.activated? && @user.authenticated?(:reset, params[:id])
+
+    redirect_to root_path
+  end
+
+  # Checks expiration of reset token.
+  def check_expiration
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = t "flash.password_reset_expired"
+    redirect_to new_password_reset_path
+  end
+end
