f1aed1a53fd42e6aa717db1c7af1c8bee4331b59
Phan Minh Huy <phanminhhuy1509@gmail.com>
2021-03-17 23:30:47 +0700

    Chapter_13: User microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    return unless logged_in?
+
+    @micropost = current_user.microposts.build
+    @feed_items = paginate current_user.feed.by_created_at
+  end
 
   def help; end
 end
